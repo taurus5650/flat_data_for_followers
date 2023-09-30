887c1e90afe815ad4ca911f16964e9f4155f5b88
taurus5650 <taurus_5650@hotmail.com>
2023-09-30 23:30:05 +0800

    no sandbox

--- followers.py
+++ followers.py
@@ -14,6 +14,8 @@ class Followers:
     def initializeDriver(self):
         options = uc.ChromeOptions()
         options.add_argument("--headless")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-gpu")
         options.add_argument(
             "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
 
