15fa4520e8cc415d46148d0f66e55b2811f75b43
taurus5650 <taurus_5650@hotmail.com>
2023-09-30 23:05:56 +0800

    chrome driver

--- followers.py
+++ followers.py
@@ -18,7 +18,7 @@ class Followers:
             "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
 
         chromeVersion = "91.0.4472.124"
-        chromeDriverPath = f"/path/to/chromedriver_{chromeVersion}"
+        chromeDriverPath = ChromeDriverManager().install()
         service = Service(chromeDriverPath)
         driver = uc.Chrome(service=service, options=options)
         return driver
